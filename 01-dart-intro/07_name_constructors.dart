07d2e156f6c2ed5baf168d90dc633e8e0d08d5e7
KevCristensen <ke.cristensen@duocuc.cl>
2023-12-22 15:16:45 -0300

    name constructors

---
+++ 01-dart-intro/07_name_constructors.dart
@@ -0,0 +1,50 @@
+void main() {
+  
+  final Map<String, dynamic> rawJson = {
+    'name': 'Tony Stark',
+    'power': 'Money',
+    'isAlive': true
+  };
+  
+  final Hero ironman = new Hero.fromJson( rawJson );
+  
+  
+//  final Hero ironman = new Hero(
+//    isAlive: rawJson['isAlive2'] ?? false,
+//    power: 'Money',
+//    name: 'Tony Stark'
+//  );
+  
+  
+//  final Hero ironman = new Hero(
+//    isAlive: false,
+//    power: 'Money',
+//    name: 'Tony Stark'
+//  );
+  
+  print(ironman);
+}
+
+class Hero {
+  
+  String name;
+  String power;
+  bool isAlive;
+  
+  Hero({
+    required this.name,
+    required this.power,
+    required this.isAlive
+  });
+  
+  Hero.fromJson( Map<String, dynamic> json ) 
+    : name = json['name'] ?? 'No name found',
+      power = json['power'] ?? 'No power found',
+      isAlive = json['isAlive'] ?? 'No isAlive found';
+  
+  
+  @override
+  String toString() {
+    return '$name, $power, isAlive: ${isAlive ? 'YES!': 'Nope'} ';
+  }
+}
